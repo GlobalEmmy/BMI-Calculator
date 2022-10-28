25bbe8b763e8af57f38698867a07a7a7d171239a
GlobalEmmy <monyem44@gmail.com>
2022-10-28 12:06:59 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.post("/", (req, res) => {
     let height = parseFloat(req.body.height)
     let bmi = Math.round(weight/(Math.pow(height, 2)))
     if (bmi < 18.5){
-         res.send(`Your bmi is: ${bmi}. You are underweight.`);
+         res.send( `<h1>Your bmi is: ${bmi}. You are underweight.</h1>`);
     }else if(bmi >= 18.5 && bmi <= 24.5 ){
-         res.send(`Your bmi is: ${bmi}. You are within the normal weight range.`);
+         res.send(`<h1>Your bmi is: ${bmi}. You are within the normal weight range.</h1>`);
     } else if(bmi >= 25 && bmi <= 29.9){
-         res.send(`Your bmi is: ${bmi}. You are within the overweight range.`);
+         res.send(`<h1>Your bmi is: ${bmi}. You are within the overweight range.</h1>`);
     }else{
-         res.send(`Your bmi is: ${bmi}. You are within the obese range.`);
+         res.send(`<h1>Your bmi is: ${bmi}. You are within the obese range.</h1>`);
 
     };  
 })
